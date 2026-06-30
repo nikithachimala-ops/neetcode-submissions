cc5aeb1740a22c4a866cd22cadc47c9dc6ffa09c
Nikitha Chimala <nikithachimala4@gmail.com>
2026-06-30 16:12:44 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int res = 0;
+        for (String d : details) {
+            if (Integer.parseInt(d.substring(11, 13)) > 60)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
